61ca7a534c6dda5e0bbb53be504182510001e874
Denis Toledo <contato@denistoledo.com.br>
2023-09-30 13:28:55 +0200

    chore: add real quotes

--- src/screens/homer-quotes/index.tsx
+++ src/screens/homer-quotes/index.tsx
@@ -2,10 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
 
 const quotesArray = [
-  "Quote 1",
-  "Quote 2",
-  "Quote 3",
-  // Add more quotes here
+  "Nobody -- that’s my name. Nobody -- so my mother and father call me, all my friends.",
+  "Her gifts were mixed with good and evil both.",
+  "Man is the vainest of all creatures that have their being upon earth.",
+  "There is a time for making speeches, and a time for going to bed.",
+  "For there is nothing better in this world than that man and wife should be of one mind in a house.",
+  "Be strong, saith my heart; I am a soldier; I have seen worse sights than this.",
+  "The proud heart feels not terror nor turns to run and it is his own courage that kills him.",
+  "I say no wealth is worth my life!",
+  "My life is more to me than all the wealth of Ilius",
+  "If only the gods are willing. They rule the vaulting skies. They’re stronger than I to plan and drive things home.",
 ];
 
 const RandomQuoteApp = () => {
