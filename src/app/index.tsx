d0b344180e5bafd7141b2fa82017136fcce51c0d
Denis Toledo <contato@denistoledo.com.br>
2024-06-29 20:04:22 +0200

    organise routes - p2

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -22,24 +22,24 @@ const query = gql`
 
 export default function IndexScreen() {
   const { data, loading, error } = useQuery(query);
-  const quotes = data?.quotes?.data;
-  const selectedQuote = quotes[0];
 
   if (error) return <Text>Error</Text>;
+  if (!data) return <Text>No data</Text>;
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
 
       {loading && <Text>Loading...</Text>}
+
       <View style={styles.quote}>
-        <Text>{selectedQuote.quote}</Text>
+        <Text>{data.quotes.data[2].quote}</Text>
       </View>
       <View style={{ padding: 50, gap: 16 }}>
         <Link
           href={{
             pathname: "/share/[id]",
-            params: { id: selectedQuote?.id },
+            params: { id: `${data.quotes.data[2].id}` },
           }}
           asChild
         >
