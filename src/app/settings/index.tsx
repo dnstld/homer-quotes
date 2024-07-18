b2791784ea128b0253bde1f482d8fb2b6a117289
Denis Toledo <contato@denistoledo.com.br>
2024-07-18 20:50:27 +0200

    update settings UI

--- src/app/settings/index.tsx
+++ src/app/settings/index.tsx
@@ -27,57 +27,55 @@ export default function SettingsScreen() {
   const openInstagram = async () => {
     const appUrl = "instagram://user?username=homerquotesapp";
     const webUrl = "https://www.instagram.com/homerquotesapp";
-    const supportedApp = await Linking.canOpenURL(appUrl);
 
-    if (supportedApp) {
-      await Linking.openURL(appUrl);
-    } else {
-      const supportedWeb = await Linking.canOpenURL(webUrl);
-      if (supportedWeb) {
-        await Linking.openURL(webUrl);
+    try {
+      const supportedApp = await Linking.canOpenURL(appUrl);
+
+      if (supportedApp) {
+        await Linking.openURL(appUrl);
       } else {
-        Alert.alert(`Don't know how to open this URL: ${webUrl}`);
+        const supportedWeb = await Linking.canOpenURL(webUrl);
+        if (supportedWeb) {
+          await Linking.openURL(webUrl);
+        } else {
+          Alert.alert(`Don't know how to open this URL: ${webUrl}`);
+        }
       }
+    } catch (error) {
+      console.error("Error opening URL: ", error);
+      Alert.alert("An error occurred", JSON.stringify(error));
     }
   };
 
   return (
     <ScrollView style={styles.container}>
       <View style={[styles.wrapper, { paddingBottom: bottom }]}>
-        <TouchableOpacity style={styles.control} onPress={openInstagram}>
-          <View style={{ flex: 1 }}>
-            <Text style={styles.itemTitle}>Follow us at @homerquotesapp</Text>
+        <TouchableOpacity style={styles.instagram} onPress={openInstagram}>
+          <View style={styles.instagramContent}>
+            <Text style={styles.instagramTitle}>
+              Follow us at @homerquotesapp
+            </Text>
             <Text>
-              Love our app? Stay connected and get more fun updates by following
-              us on Instagram!
+              Enjoying our app? Stay connected and get more fun updates by
+              following us on Instagram!
             </Text>
           </View>
           <Ionicons name="logo-instagram" size={32} />
         </TouchableOpacity>
+
         <View>
           <Text style={styles.title}>Controls</Text>
 
           {granted && (
             <TouchableOpacity onPress={toggleSwitch}>
-              <View
-                style={{
-                  flexDirection: "row",
-                  gap: 16,
-                  flex: 1,
-                  alignItems: "center",
-                  backgroundColor: "#F8659F",
-                  padding: 16,
-                  borderRadius: 8,
-                  marginBottom: 16,
-                }}
-              >
+              <View style={styles.notificationWarning}>
                 <Ionicons name="warning-outline" size={48} color={"white"} />
-                <View style={{ flex: 1, gap: 8 }}>
-                  <Text style={{ color: "white" }}>
+                <View style={styles.notificationWarningContent}>
+                  <Text style={styles.notificationWarningText}>
                     Enable push notifications for a dose of Homer’s humor. We’ll
                     send quotes occasionally to keep you smiling!
                   </Text>
-                  <Text style={{ color: "white", fontWeight: "bold" }}>
+                  <Text style={styles.notificationWarningButton}>
                     Tap to enable
                   </Text>
                 </View>
@@ -87,7 +85,7 @@ export default function SettingsScreen() {
           <View style={styles.controls}>
             <View style={styles.control}>
               <View>
-                <Text style={styles.itemTitle}>Push notifications</Text>
+                <Text style={styles.controlTitle}>Push notifications</Text>
                 <Text>Notifications designed to bring joy to your day</Text>
               </View>
               <Switch onValueChange={toggleSwitch} value={isEnabled} />
@@ -96,128 +94,95 @@ export default function SettingsScreen() {
         </View>
 
         <View>
-          <View>
-            <Text style={styles.title}>About</Text>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: "#00AAFF",
-                padding: 16,
-                borderRadius: 8,
-                marginBottom: 16,
-              }}
-            >
-              <View
-                style={{
-                  flexDirection: "row",
-                  gap: 16,
-                  flex: 1,
-                  alignItems: "center",
-                }}
-              >
-                <HomerAvatarSvg style={styles.avatar} />
-                <View style={{ flex: 1 }}>
-                  <Text style={{ color: "white" }}>
-                    This app is a fan-made project and is not affiliated with or
-                    endorsed by Matt Groening, Gracie Films, 20th Television, or
-                    Disney Plus.
-                  </Text>
-                </View>
+          <Text style={styles.title}>About</Text>
+          <View style={styles.about}>
+            <Text>
+              Explore funny Homer Simpson quotes from every season of The
+              Simpsons. Share the fun and laughter with friends! D'oh!
+            </Text>
+
+            <View style={styles.note}>
+              <HomerAvatarSvg style={styles.noteAvatar} />
+              <View style={styles.noteContent}>
+                <Text style={styles.noteText}>
+                  This app is a fan-made project and is not affiliated with or
+                  endorsed by the original creators or any associated companies.
+                </Text>
               </View>
             </View>
             <Text>
-              Homer Quotes App, your go-to source for daily laughter and wisdom
-              from everyone's favorite animated dad, Homer Simpson! As lifelong
-              "The Simpsons" fans, we've poured our love for the show into this
-              app to bring you the funniest and most memorable quotes from Homer
-              J. Simpson.
+              We've created this app to pay tribute to "The Simpsons,"
+              celebrating its humor and heart. Although we don't own the rights
+              to the characters or quotes, our app is dedicated to the show that
+              has given us countless laughs and unforgettable moments. After
+              watching 35 seasons and 768 episodes, spanning more than 1765
+              hours of pure joy, we hope to bring the show's spirit to fans
+              everywhere
+            </Text>
+            <Text>
+              We thank Matt Groening, "The Simpsons" creators, and Disney Plus
+              for keeping the series accessible. This app wouldn't be possible
+              without "The Simpsons" inspiration and joy.
             </Text>
           </View>
-
-          <View style={styles.dashboard}>
-            <View style={styles.dashboardItem}>
-              <Text style={styles.dashboardData}>35</Text>
-              <Text style={styles.dashboardName}>seasons</Text>
-            </View>
-            <View style={styles.dashboardItem}>
-              <Text style={styles.dashboardData}>768</Text>
-              <Text style={styles.dashboardName}>episodes</Text>
-            </View>
-            <View style={styles.dashboardItem}>
-              <Text style={styles.dashboardData}>1765</Text>
-              <Text style={styles.dashboardName}>quotes</Text>
-            </View>
-          </View>
         </View>
 
-        <View>
-          <Text style={styles.title}>Our Vision</Text>
-          <Text>
-            As fans who have watched every episode of "The Simpsons" on Disney
-            Plus, we wanted to create an app that celebrates the humor and heart
-            of the series. While we don't own the rights to the characters or
-            quotes, we've developed this app as a tribute to the show that has
-            given us countless laughs and unforgettable moments.
-          </Text>
-        </View>
-
-        <View>
-          <Text style={styles.title}>Acknowledgments</Text>
-          <Text>
-            We extend our heartfelt thanks to Matt Groening, the creative minds
-            behind "The Simpsons," and Disney Plus for keeping the series
-            accessible to fans. This app wouldn't be possible without the
-            inspiration and joy "The Simpsons" has brought into our lives.
-          </Text>
-        </View>
-
-        <Text>Developed by Denis Toledo.</Text>
+        <Text style={styles.footer}>
+          Developed and maintained by Denis Toledo | 2024
+        </Text>
       </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  avatar: {
-    width: 48,
-    height: 48,
-  },
-  dashboard: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    gap: 8,
-    marginTop: 16,
-  },
-  dashboardItem: {
-    alignItems: "center",
-    backgroundColor: "white",
-    padding: 16,
-    borderRadius: 16,
-    flex: 1,
-  },
-  dashboardData: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  dashboardName: {
-    fontSize: 12,
-    color: "#767577",
-  },
   container: {
     flex: 1,
     paddingHorizontal: 16,
     padding: 16,
   },
   wrapper: {
-    gap: 16,
+    gap: 24,
   },
   title: {
     fontWeight: "bold",
     marginBottom: 8,
   },
-  itemTitle: {
+  instagram: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+  },
+  instagramContent: {
+    flex: 1,
+  },
+  instagramTitle: {
     fontWeight: "bold",
     marginBottom: 4,
+  },
+  notificationWarning: {
+    flexDirection: "row",
+    gap: 16,
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#F8659F",
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  notificationWarningContent: {
+    flex: 1,
+    gap: 8,
+  },
+  notificationWarningText: {
+    color: "white",
+  },
+  notificationWarningButton: {
+    color: "white",
+    fontWeight: "bold",
   },
   controls: {
     gap: 8,
@@ -229,5 +194,35 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 16,
     borderRadius: 8,
+  },
+  controlTitle: {
+    fontWeight: "bold",
+    marginBottom: 4,
+  },
+  about: {
+    gap: 16,
+  },
+  note: {
+    flex: 1,
+    backgroundColor: "#00AAFF",
+    padding: 16,
+    borderRadius: 8,
+    flexDirection: "row",
+    gap: 16,
+    alignItems: "center",
+  },
+  noteAvatar: {
+    width: 48,
+    height: 48,
+  },
+  noteContent: {
+    flex: 1,
+  },
+  noteText: {
+    color: "white",
+  },
+  footer: {
+    textAlign: "center",
+    fontStyle: "italic",
   },
 });
