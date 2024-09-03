2bea838e7f959106af8900be6676457a6d3dcec0
Denis Toledo <contato@denistoledo.com.br>
2024-09-03 09:34:14 +0200

    chore: prod test

--- src/hooks/NotificationSettings.tsx
+++ src/hooks/NotificationSettings.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState, useCallback } from "react";
 import { startActivityAsync, ActivityAction } from "expo-intent-launcher";
-import { AppState, Linking, Platform } from "react-native";
+import { Alert, AppState, Linking, Platform } from "react-native";
 import * as Notifications from "expo-notifications";
 
-import { usePushNotifications } from "./Notifications";
+// import { usePushNotifications } from "./Notifications";
 
 type NotificationSettings = Notifications.NotificationPermissionsStatus;
 
@@ -15,32 +15,37 @@ type UseNotificationSettingsReturn = [
 
 export const useNotificationSettings = (): UseNotificationSettingsReturn => {
   const [settings, setSettings] = useState<NotificationSettings>();
-  const {
-    expoPushToken,
-    registerForPushNotifications,
-    unregisterForPushNotifications,
-  } = usePushNotifications();
+  // const {
+  //   expoPushToken,
+  //   registerForPushNotifications,
+  //   unregisterForPushNotifications,
+  // } = usePushNotifications();
 
   useEffect(() => {
     const handleRegister = async () => {
       if (settings?.status === "denied") {
-        if (expoPushToken) {
-          unregisterForPushNotifications(expoPushToken);
-        }
+        Alert.alert("Settings status: denied");
+        // if (expoPushToken) {
+        //   unregisterForPushNotifications(expoPushToken);
+        // }
       } else if (
         settings?.status === "granted" ||
         settings?.ios?.status ===
           Notifications.IosAuthorizationStatus.PROVISIONAL
       ) {
-        registerForPushNotifications();
+        // registerForPushNotifications();
+        Alert.alert("Settings status: granted");
       }
     };
 
     const getSettings = async () => {
       try {
         const settings = await Notifications.getPermissionsAsync();
+        Alert.alert("getSettings: settings ", JSON.stringify(settings));
         setSettings(settings);
       } catch (error) {
+        // @ts-ignore
+        Alert.alert("getSettings: error ", error.message);
         throw new Error(`Failed to get notification settings: ${error}`);
       } finally {
         handleRegister();
